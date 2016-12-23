3a5c33730a42207075949168c538cdb89bb9787a
Majkel44 <m.werner9205@gmail.com>
2016-12-23 21:49:03 +0100

    - added argument to constructor - added QextSerialPort object - added method for opening serialport - added method for writing data through serialport

--- rgbcontroller.h
+++ rgbcontroller.h
@@ -1,6 +1,7 @@
 #ifndef RGBCONTROLLER_H
 #define RGBCONTROLLER_H
 #include <QObject>
+#include <3rdparty/qextserialport/src/qextserialport.h>
 
 
 
@@ -9,10 +10,16 @@ class RGBController : QObject
 {
     Q_OBJECT
 public:
-    RGBController();
+    RGBController(QString port_name);
     void setRGB(int r,int g,int b);
     void setHSV(int h,int s,int v);
     void setAnimation(int mode,unsigned long speed,int step);
+
+private:
+    QextSerialPort *port;
+    void open_serial_port(QString port_name);
+    void write_to_serial_port(QString data);
+
 
 };
 
